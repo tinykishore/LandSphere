263cc0ee1987d03bb041d7dde16d6699e078d0a0
Kishorè Shanto <tinykishore@gmail.com>
2023-04-10 13:07:13 +0600

    card added

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8">f
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../dist/output.css" rel="stylesheet">
     <title>LandSphere | Your Personal Land Manager</title>
